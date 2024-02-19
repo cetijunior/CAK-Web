eab9417d2c651ec5e019d7cc2522e76433847adc
archieQa <luarsarsi@gmail.com>
2024-02-19 21:45:05 +0100

    I want 8grand

--- cak/pages/index.tsx
+++ cak/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p> Get started by editing&nbsp;
+        <p> I want 8grand
           <code className="font-mono font-bold">pages/index.tsx</code>
         </p>
       </div>
