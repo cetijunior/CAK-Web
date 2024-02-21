2b0d12774606770f96eb0bf860527583148aab78
archieQa <luarsarsi@gmail.com>
2024-02-21 21:22:09 +0100

    added the love of my life

--- cak/pages/portfolio/Portfolio.tsx
+++ cak/pages/portfolio/Portfolio.tsx
@@ -3,20 +3,22 @@ import {projects, Project } from './portfolioDb';
 
 const Portfolio: React.FC = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4">
-        {projects.map((project: Project) => (
-          <div key={project.id} className="bg-emerald-800 rounded-lg shadow-lg overflow-hidden">
-            <img src={project.imageUrl} alt={project.title} className="w-full h-50 object-cover" />
-            <div className="p-4">
-              <h3 className="text-lg font-bold">{project.title}</h3>
-              <p className="text-sm text-white-600">{project.description}</p>
+    <section id='portfolio'>
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4">
+          {projects.map((project: Project) => (
+            <div key={project.id} className="bg-emerald-800 rounded-lg shadow-lg overflow-hidden">
+              <img src={project.imageUrl} alt={project.title} className="w-full h-50 object-cover" />
+              <div className="p-4">
+                <h3 className="text-lg font-bold">{project.title}</h3>
+                <p className="text-sm text-white-600">{project.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    </section>
+     );
 };
 
 export default Portfolio;
