b043225977b00821e2c50a567f252fa0ee522de4
kenidervishaj <krenar.dervishaj@fau.de>
2024-02-23 20:40:51 +0100

    Design and resposnivness added to services and offers

--- cak/pages/services/Offers.tsx
+++ cak/pages/services/Offers.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close';
 import { Card, CardHeader, CardBody, Image } from '@nextui-org/react';
 
-const Offers = ({children, isOpen, onClose}) => {
-  if (!isOpen) return null;
+type OffersProps = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Offers: React.FC<OffersProps> = ({children, isOpen, onClose}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(isOpen);
+  }, [isOpen]);
+
+  const handleClose = () => {
+    setIsVisible(false);
+    setTimeout(onClose, 500);
+  };
+
   return ( 
     <div style={{
       position: 'fixed',
@@ -16,9 +32,12 @@ const Offers = ({children, isOpen, onClose}) => {
       justifyContent: 'center',
       alignItems: 'center',
       zIndex: 1000,
+      transition: 'opacity 0.5s',
+      opacity: isVisible ? 1 : 0,
+      pointerEvents: isVisible ? 'auto' : 'none',
     }}>
       <div style={{
-        backgroundColor: '#064e3b',
+        backgroundColor: '#f8edeb',
         marginTop: '80px',
         width: '1200px',
         height: '550px',
@@ -27,16 +46,19 @@ const Offers = ({children, isOpen, onClose}) => {
         overflow: 'auto',
         scrollbarWidth: 'none',
         zIndex: 1001,
+        transition: 'opacity 0.5s',
+        opacity: isVisible ? 1 : 0,
+        pointerEvents: isVisible ? 'auto' : 'none',
       }}>
         {children}
-        <button className="text-white p-2 ml-2 mt-2 bg-black rounded-full" onClick={onClose}><CloseIcon/></button>
-        <Card className='ml-2 mr-2 mt-2 md:mx-10 rounded-xl md:hover:drop-shadow-2xl overflow-hidden bg-emerald-300'>
+        <button className="text-white p-2 ml-2 mt-2 bg-[#381d1d] rounded-full" onClick={onClose}><CloseIcon/></button>
+        <Card className='ml-2 mr-2 mt-2 md:mx-10 rounded-3xl drop-shadow-2xl overflow-hidden bg-[#e8e8e8]'>
           <CardHeader className='pt-2 px-4 flex-col items-start'>
-            <p className='ml-2 mt-2 text-3xl font-extrabold text-black'>Our Offer</p>
-            <h4 className='ml-2 text-xl text-black font-bold mt-4'>The 4-in-1 Bundle</h4>
+            <p className='mt-2 text-3xl font-extrabold text-[#5D1F20]'>Our Offer</p>
+            <h4 className='ext-xl text-[#5D1F20] font-bold mt-4'>The 4-in-1 Bundle</h4>
           </CardHeader>
             <CardBody className='flex flex-col md:flex-row justify-between items-center md:w-full overflow-y-auto max-h-490'>
-              <p className='text-base text-black max-w-xl mb-4 md:mb-0'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus id blanditiis quis voluptas quasi molestias assumenda aperiam perferendis veniam? Officia, saepe sunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores qui quae ea, temporibus laboriosam, ex mollitia accusantium aspernatur quibusdam aliquam quaerat odit minima illo vel voluptatum facere ipsum aut.Cupiditate ea illum deserunt culpa ducimus aspernatur neque.Quos reprehenderit, voluptate facere dolores praesentium esse dicta quas, saepe eveniet aspernatur, corrupti nesciunt quisquam velit possimus cumque mollitia consequuntur tenetur facilis!</p>
+              <p className='text-base text-[#381d1d] max-w-xl mb-4 md:mb-0'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus id blanditiis quis voluptas quasi molestias assumenda aperiam perferendis veniam? Officia, saepe sunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores qui quae ea, temporibus laboriosam, ex mollitia accusantium aspernatur quibusdam aliquam quaerat odit minima illo vel voluptatum facere ipsum aut.Cupiditate ea illum deserunt culpa ducimus aspernatur neque.Quos reprehenderit, voluptate facere dolores praesentium esse dicta quas, saepe eveniet aspernatur, corrupti nesciunt quisquam velit possimus cumque mollitia consequuntur tenetur facilis!</p>
               <Image src='https://www.appsdevpro.com/blog/wp-content/uploads/2022/09/Top-Web-Design-Development-Trends-You-Need-to-Know-in-2023.png' width={500} height={300} className='rounded-xl max-w-full h-auto'></Image>
           </CardBody>
         </Card>
